16b3c04528914b41586b038b210b8b3d10b81f25
Sheila <sajbalboa@hotmail.com>
2024-09-07 22:22:14 -0500

    actualizacion

---
+++ src/main/java/org/example/tarea2/controller/EmployeesController.java
@@ -0,0 +1,34 @@
+package org.example.tarea2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EmployeesController {
+
+
+    //private EmployeeDAO employeesDAO = new EmployeeDAO();
+    //private JobDAO  jobsDAO = new JobDAO();
+
+    @GetMapping({"employee/list", "employee"})
+    public String listarEmpleados() {
+//        COMPLETAR
+        return "XXXXXX";
+    }
+
+    @GetMapping("")
+    public String informEmpleado() {
+        //        COMPLETAR
+        return "XXXXXX";
+    }
+
+    @GetMapping("")
+    public String borrarEmpleado(Model model) {
+        model.addAttribute("tipo_alert", "success");
+        model.addAttribute("msg", "Se borro el empleado");
+//        COMPLETAR
+        return "XXXXXX";
+    }
+}
---
+++ src/main/java/org/example/tarea2/controller/HomeController.java
@@ -0,0 +1,16 @@
+package org.example.tarea2.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String irAlMain() {
+        return "redirect:/employee";
+    }
+
+}
